6fa7b7457c22cdad34da298209a50959739115ac
Tunmise Ogunniyi <ogunniyitunmise@gamil.com>
2020-01-08 12:26:40 +0100

    Remove unneccessary files

--- src/http/start.js
+++
@@ -1,7 +0,0 @@
-import app from './app';
-import config from './config';
-import { log } from '../utils/logger';
-
-export default app.listen(config.port, () => {
-  log.info(`App listening on port ${config.port}`);
-});
