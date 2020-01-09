204e3ea35bbdd352535b6ff7082d52ac831e19fa
Tunmise Ogunniyi <ogunniyitunmise@gamil.com>
2020-01-09 10:36:20 +0100

    Update device list

--- public/js/devices.js
+++ public/js/devices.js
@@ -1,8 +1,8 @@
 // eslint-disable-next-line no-unused-vars
 const deviceIdToLabel = {
-  1: 'Device A',
-  2: 'Device B',
-  3: 'Device C',
-  4: 'Device D',
-  5: 'Device E',
+  sample1: 'Device A',
+  sample2: 'Device B',
+  sample3: 'Device C',
+  sample4: 'Device D',
+  sample5: 'Device E',
 };
